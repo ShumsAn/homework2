2f25f97eaa763f75af333e3f26123ef7d2837b64
ShumsAn <shum-sol@outlook.com>
2024-07-15 16:16:57 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+quantity_HW = 12
+hours_spent = 1.5
+course_name = 'Python'
+average_time = hours_spent / quantity_HW
+print(course_name,', всего задач:',quantity_HW,', затрачено часов:',hours_spent,', среднее время выполнения:',average_time ,' часа',sep='')
+
